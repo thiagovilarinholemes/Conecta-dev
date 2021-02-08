06092000466f551d643cc4b3b07e1b465fc6416d
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-02-08 13:08:03 -0300

    Notifications

--- src/pages/Home/components/Header/Notifications.js
+++ src/pages/Home/components/Header/Notifications.js
@@ -3,13 +3,44 @@ import { useSelector, useDispatch } from 'react-redux'; // useSelector busca dad
 import SvgIcon from '@material-ui/core/SvgIcon';
 import IconButton from '@material-ui/core/IconButton';
 import Popover from '@material-ui/core/Popover';
-import { Bell } from 'react-feather';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles'
+import {
+    Bell as BellIcon,
+    Star as StarIcon,
+    MessageCircle as MessageIcon,
+    Heart as HeartIcon,
+    Users as ConnectionIcon,
+} from 'react-feather';
+
 import { getNotifications } from '../../../../actions/notificationsActions';
+
+const iconsMap = {
+    reviews: StarIcon,
+    new_comment: MessageIcon,
+    like: HeartIcon,
+    connection: ConnectionIcon,
+};
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        background: theme.palette.secondary.main,
+        color: theme.palette.secondary.contrastText
+    }
+}));
 
 function Notifications() {
 
+    const classes = useStyles();
+
     const account = useSelector(state => state.account);
-    const notifications = useSelector(state => state.notifications);
+    const notifications = useSelector(state => state.notifications.notifications);
     const isAuthenticated = !!account.user;
     const ref = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +53,7 @@ function Notifications() {
     const handleClose = () => {
         setIsOpen(false);
     }
-    
+
     useEffect(() => {
         dispatch(getNotifications());
     }, [dispatch]);
@@ -34,7 +65,7 @@ function Notifications() {
             <React.Fragment>
                 <IconButton ref={ref} onClick={handleOpen}>
                     <SvgIcon>
-                        <Bell />
+                        <BellIcon />
                     </SvgIcon>
                 </IconButton>
                 <Popover
@@ -50,7 +81,31 @@ function Notifications() {
                         horizontal: 'right',
                     }}
                 >
-                    The content of the Popover.
+                    <Box p={2}>
+                        <Typography variant="h6" color="textPrimary">
+                            Notificações
+                        </Typography>
+                    </Box>
+
+                    <List>
+                        {
+                            notifications.map((notification) => {
+                                const Icon = iconsMap[notification.type]
+                                return (
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        <Avatar className={classes.icon}>
+                                            <SvgIcon> 
+                                                <Icon />                                            
+                                            </SvgIcon>
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText primary={notification.title} primaryTypographyProps={{ variant: 'subtitle2', color: 'textPrimary' }} secondary={notification.description} />
+                                </ListItem>
+                                )
+                            })
+                        }
+                    </List>
                 </Popover>
             </React.Fragment>
         )
